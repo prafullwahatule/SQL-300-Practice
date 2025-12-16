fbcfca86b5ac201e698358912c78ebac83c89cd5
Prafull Wahatule <prafullwahatule@gmail.com>
2025-12-17 00:52:27 +0530

    Solve 31 to 35 Q

--- Advanced (Q201–Q300)/C.CTEs (Common Table Expressions) (Q31–Q50).sql
+++ Advanced (Q201–Q300)/C.CTEs (Common Table Expressions) (Q31–Q50).sql
@@ -20,9 +20,63 @@ JOIN customers c
     ON c.customer_id = t.customer_id;
 
 -- Q32. Find avg order value per month using CTE.
+WITH avg_order_value_cte AS (
+    SELECT
+        YEAR(order_date) AS order_year,
+        MONTH(order_date) AS order_month,
+        AVG(total_amount) AS avg_order_value
+    FROM orders
+    GROUP BY YEAR(order_date), MONTH(order_date)
+)
+SELECT *
+FROM avg_order_value_cte;
+
 -- Q33. Show top 3 customers by revenue using CTE.
+WITH customer_revenue AS (
+    SELECT
+        c.customer_id,
+        CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+        SUM(o.total_amount) AS total_revenue
+    FROM customers c
+    JOIN orders o ON o.customer_id = c.customer_id
+    GROUP BY c.customer_id, c.first_name, c.last_name
+)
+SELECT *
+FROM customer_revenue
+ORDER BY total_revenue DESC
+LIMIT 3;
+
 -- Q34. Find pizza category revenue using CTE.
+WITH category_revenue AS (
+    SELECT
+        p.category,
+        SUM(p.price * oi.quantity) AS total_revenue
+    FROM pizzas p
+    JOIN order_items oi ON oi.pizza_id = p.pizza_id
+    GROUP BY p.category
+)
+SELECT *
+FROM category_revenue;
+
 -- Q35. Show employee with max deliveries using CTE.
+WITH delivery_count AS (
+    SELECT 
+        e.employee_id,
+        e.name,
+        COUNT(d.delivery_id) AS total_deliveries
+    FROM employees e
+    JOIN deliveries d 
+        ON d.employee_id = e.employee_id
+    GROUP BY e.employee_id, e.name
+),
+max_delivery AS (
+    SELECT MAX(total_deliveries) AS max_del
+    FROM delivery_count
+)
+SELECT *
+FROM delivery_count
+WHERE total_deliveries = (SELECT max_del FROM max_delivery);
+
 -- Q36. Use recursive CTE to show hierarchy of employees (Manager → Chef → Delivery Boy).
 -- Q37. Find customers who ordered more than 5 pizzas using CTE.
 -- Q38. Show revenue per city using CTE.
