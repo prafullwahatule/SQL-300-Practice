3fa55d28947316dcacd5c8ffcaf4e1951369c71d
Prafull Wahatule <prafullwahatule@gmail.com>
2025-12-21 10:51:46 +0530

    Solve -- F. Views, Procedures, Triggers (Q86–Q95)

--- Advanced (Q201–Q300)/F.Views, Procedures, Triggers (Q86–Q95).sql
+++ Advanced (Q201–Q300)/F.Views, Procedures, Triggers (Q86–Q95).sql
@@ -110,7 +110,100 @@ DELIMITER ;
 CALL insert_new_pizza(101, 'Farmhouse Pizza', 'Veg', 'M', 299.00);
 	
 -- Q91. Write a procedure to get monthly revenue trend.
+DELIMITER $$
+
+CREATE PROCEDURE monthly_revenue()
+BEGIN 
+	SELECT
+		MONTHNAME(o.order_date) AS Month_Name,
+        SUM(oi.quantity * p.price) AS Revenue
+	FROM orders o
+    JOIN order_items oi
+    ON o.order_id = oi.order_id
+    JOIN pizzas p
+    ON p.pizza_id = oi.pizza_id
+    GROUP BY MONTHNAME(o.order_date);
+    
+END $$
+
+DELIMITER ;
+
+-- Call
+CALL monthly_revenue();
+
 -- Q92. Write a trigger to update total_amount when order_items change.
+DELIMITER $$
+
+CREATE TRIGGER trg_order_items_insert
+AFTER INSERT ON order_items
+FOR EACH ROW
+BEGIN
+    UPDATE orders o
+    SET o.total_amount = (
+        SELECT SUM(oi.quantity * p.price)
+        FROM order_items oi
+        JOIN pizzas p ON oi.pizza_id = p.pizza_id
+        WHERE oi.order_id = NEW.order_id
+    )
+    WHERE o.order_id = NEW.order_id;
+END $$
+
+DELIMITER ;
+
 -- Q93. Write a trigger to log cancelled deliveries.
+DELIMITER $$
+
+CREATE TRIGGER trg_cancelled_deliveries
+AFTER UPDATE ON deliveries
+FOR EACH ROW
+BEGIN
+    IF NEW.status = 'Cancelled' AND OLD.status <> 'Cancelled' THEN
+        INSERT INTO cancelled_delivery_log (
+            delivery_id,
+            order_id
+        )
+        VALUES (
+            NEW.delivery_id,
+            NEW.order_id
+        );
+    END IF;
+END $$
+
+DELIMITER ;
+
 -- Q94. Write a view to show employee performance (deliveries count).
+CREATE VIEW emp_performance AS
+SELECT
+    e.employee_id,
+    e.name,
+    COUNT(d.delivery_id) AS deliverie_count
+FROM employees e
+JOIN deliveries d
+    ON d.employee_id = e.employee_id
+GROUP BY e.employee_id, e.name;
+
+SELECT * 
+FROM emp_performance
+ORDER BY deliverie_count DESC;
+
 -- Q95. Write a view to show customer loyalty (active vs inactive).
+CREATE VIEW cust_loyalty AS
+SELECT
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    COUNT(o.order_id) AS total_orders,
+    CASE
+        WHEN COUNT(o.order_id) >= 1 THEN 'Active'
+        ELSE 'Inactive'
+    END AS loyalty_status
+FROM customers c
+LEFT JOIN orders o
+    ON o.customer_id = c.customer_id
+GROUP BY
+    c.customer_id,
+    c.first_name,
+    c.last_name;
+    
+SELECT * FROM cust_loyalty;
+
+    
